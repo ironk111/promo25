c6d48c0b019fa9375296545c936edcd4c7330f9d
Ironk <pepe@gmail.com>
2024-09-10 23:44:54 -0300

    ADD: product cards with J. Logo. Theme

--- public/index.js
+++ public/index.js
@@ -1,4 +1,23 @@
-const name = "Batería";
-document.querySelector("h5").innerText = name;
+let productos = "Productos";
+document.querySelector("h1").innerText = productos;
 
-console.log(name)
+
+let array = [];
+
+for (let cards = 1; cards < 10; cards++) {
+    const card = `<div class="col">
+      <div class="card h-100">
+        <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${cards}.jpg" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">content. This content is a little bit longer.</p>
+          <a href="#" class="btn btn-primary rounded-pill">Comprar</a>
+        </div>
+      </div>
+    </div>`
+    array.push(card)
+}
+
+document.querySelector("section").innerHTML = array.join(""); //join : convierte array en string.
+
+// https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${cards}.jpg
